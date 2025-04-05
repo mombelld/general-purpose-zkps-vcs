93f7e9cb3a826f41a2f60cf7490a0d98cc13ff55
mdamiano <mdamiano@student.ethz.ch>
2025-04-05 10:42:57 +0200

    fixed for older python version

--- src/python_util/gen_input.py
+++ src/python_util/gen_input.py
@@ -19,25 +19,27 @@ CHUNK_SIZE = 32
 HASH_WIDTH = 16
 FIX_STRING_SIZE = 15
 
+
 def decode_vc(vc_path):
     with open(vc_path, "r") as vc_file:
         vc = vc_file.read()
         # Decode payload
         encoded_payload = vc.split(".")[1]
-        decoded_payload =  base64.urlsafe_b64decode(encoded_payload + '==').decode('utf-8')
+        decoded_payload = base64.urlsafe_b64decode(
+            encoded_payload + '==').decode('utf-8')
         payload = json.loads(decoded_payload)
         # Decode disclosures
         disclosures = {}
         encoded_disclosures_list = vc.split("~")[1:-1]
         for e in encoded_disclosures_list:
-            decoded_disclosure =  base64.urlsafe_b64decode(e + '==').decode('utf-8')
+            decoded_disclosure = base64.urlsafe_b64decode(
+                e + '==').decode('utf-8')
             disclosure = json.loads(decoded_disclosure)
             disclosure_name = disclosure[1]
             disclosures[disclosure_name] = disclosure[2]
 
-
         return payload | disclosures
-    
+
 
 def add_auxiliary_input(aux_input_path, input_data):
     with open(aux_input_path, "r") as aux_input_file:
@@ -46,31 +48,31 @@ def add_auxiliary_input(aux_input_path, input_data):
         for ai in aux_input.keys():
             input_data[ai] = aux_input[ai]
 
-    
     input_data["now_timestamp"] = str(int(time.time()))
 
     return input_data
+
 
 def get_proof_inputs(proof_metadata_path):
     with open(proof_metadata_path, "r") as proof_metadata_file:
         proof_metadata = json.loads(proof_metadata_file.read())
         return proof_metadata["inputs"], proof_metadata["order"], proof_metadata["type"], proof_metadata["prefix"]
-    
 
 
 def encode_value(value, typ, pad=True, date_offset=0):
     if typ == "number":
         return [int(value)]
-    
+
     elif typ == "string":
         byte_rep = binascii.hexlify(value.encode("utf-8"))
         l = len(byte_rep) // 2
-        n_chunks= l // CHUNK_SIZE
+        n_chunks = l // CHUNK_SIZE
         rest = l % CHUNK_SIZE
 
         out = []
         for i in range(n_chunks):
-            out.append(int(byte_rep[i * CHUNK_SIZE * 2: (i + 1) * CHUNK_SIZE * 2], 16))
+            out.append(
+                int(byte_rep[i * CHUNK_SIZE * 2: (i + 1) * CHUNK_SIZE * 2], 16))
 
         if rest > 0:
             out.append(int(byte_rep[n_chunks * CHUNK_SIZE * 2:], 16))
@@ -81,18 +83,18 @@ def encode_value(value, typ, pad=True, date_offset=0):
                 out.append(0)
 
         return out
-                
+
     elif typ == "date" or typ == "date-time":
         if isinstance(value, int):
             return [value + date_offset]
         else:
             dt = date_parser.parse(value)
-            timestamp_utc = dt.replace(tzinfo=datetime.timezone.utc).timestamp()
+            timestamp_utc = dt.replace(
+                tzinfo=datetime.timezone.utc).timestamp()
             return [int(timestamp_utc) + date_offset]
     else:
         return None
-    
-    
+
 
 def encode_claims(claims, order, types, date_offset):
     encoded_names = []
@@ -115,15 +117,18 @@ def encode_claims(claims, order, types, date_offset):
 
     return encoded_names, encoded_values, hashed_claims
 
+
 def load_status_list(status_list_name):
     base_path = os.path.dirname(os.path.abspath(__file__))
-    status_list_path = os.path.join(base_path, "..", "..", "status_list", status_list_name)
+    status_list_path = os.path.join(
+        base_path, "..", "..", "status_list", status_list_name)
 
     status_list = b""
     with open(status_list_path, "r") as status_list_file:
         status_list = status_list_file.read()
 
-    n_chunks = gen_status_list.TOTAL_SIZE // INT_SIZE + (1 if gen_status_list.TOTAL_SIZE % INT_SIZE != 0 else 0)
+    n_chunks = gen_status_list.TOTAL_SIZE // INT_SIZE + \
+        (1 if gen_status_list.TOTAL_SIZE % INT_SIZE != 0 else 0)
     status_list_chunk = [0 for _ in range(n_chunks)]
 
     chunk_length = CHUNK_SIZE * 2
@@ -132,6 +137,7 @@ def load_status_list(status_list_name):
         status_list_chunk[i] = str(int("0x" + chunk, 16))
 
     return status_list_chunk
+
 
 def hardware_binding(hb_key_pair_path, input_data, prefix):
     hb_key_par_file = None
@@ -149,11 +155,11 @@ def hardware_binding(hb_key_pair_path, input_data, prefix):
     (r, s) = p256.sign(sk, nonce)
 
     h = SHA256.new(nonce)
-    z = int.from_bytes(h.digest()[:order_bytes])
+    z = int.from_bytes(h.digest()[:order_bytes], byteorder="big")
 
     G = p256.EccPoint(p256.Gx, p256.Gy)
-    u1 = (z * pow(s, -1, order))  % order
-    u2 = (r * pow(s, -1, order))  % order
+    u1 = (z * pow(s, -1, order)) % order
+    u2 = (r * pow(s, -1, order)) % order
 
     Q = sk * G
     R = u1 * G + u2 * Q
@@ -161,7 +167,6 @@ def hardware_binding(hb_key_pair_path, input_data, prefix):
     T = pow(r, -1, order) * R
     tmp = (pow(r, -1, order) * z) % order
     U = -(tmp * G)
-
 
     pwrs = gen_precomp_t.gen_precomp_t_arr(T)
 
@@ -178,15 +183,17 @@ def hardware_binding(hb_key_pair_path, input_data, prefix):
 
 def main():
     proof_path = sys.argv[1]
-    n_credentials =  int(sys.argv[2])
+    n_credentials = int(sys.argv[2])
     credential_paths = ["" for _ in range(n_credentials)]
     for i in range(n_credentials):
         credential_paths[i] = sys.argv[i + 3]
 
     input_data = {}
-    proof_inputs_l, inputs_order_l, input_types_l, prefix_l = get_proof_inputs(os.path.join(proof_path, "proof_metadata.json"))
+    proof_inputs_l, inputs_order_l, input_types_l, prefix_l = get_proof_inputs(
+        os.path.join(proof_path, "proof_metadata.json"))
 
-    input_data = add_auxiliary_input(os.path.join(proof_path, "auxiliary_inputs.json"), input_data)
+    input_data = add_auxiliary_input(os.path.join(
+        proof_path, "auxiliary_inputs.json"), input_data)
     for i, credential_path in enumerate(credential_paths):
         claims = decode_vc(credential_path)
         proof_inputs = proof_inputs_l[i]
@@ -196,10 +203,12 @@ def main():
 
         date_offset = claims["date_offset"]
         status_list_uri = claims["status_list_uri"]
-        claim_names, claim_values, hashed_claims = encode_claims(claims, inputs_order, input_types, date_offset)
+        claim_names, claim_values, hashed_claims = encode_claims(
+            claims, inputs_order, input_types, date_offset)
         for inp in proof_inputs:
             if inp == "status_list_uri":
-                input_data[f"{prefix}status_list"] = load_status_list(status_list_uri)
+                input_data[f"{prefix}status_list"] = load_status_list(
+                    status_list_uri)
             if inp in inputs_order:
                 idx = inputs_order.index(inp)
                 claim_name = claim_names[idx]
@@ -214,12 +223,12 @@ def main():
             else:
                 input_data[f"{prefix}{inp}"] = claims[inp]
 
-       
         input_data[f"{prefix}hashed_claims"] = hashed_claims
 
         # Check if we're doing hardware binding and if so add nonce signature and additional data
         if "cnf_jwk_x" in proof_inputs:
-            input_data = hardware_binding(os.path.join(proof_path, "hb_keypair.json"), input_data, prefix)
+            input_data = hardware_binding(os.path.join(
+                proof_path, "hb_keypair.json"), input_data, prefix)
 
     with open(os.path.join(proof_path, "input.json"), "w") as input_file:
         input_file.write(json.dumps(input_data))
--- src/python_util/p256.py
+++ src/python_util/p256.py
@@ -8,6 +8,7 @@ n = 0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551
 Gx = 0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296
 Gy = 0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5
 
+
 class EccPoint():
 
     def __init__(self, x, y):
@@ -28,7 +29,7 @@ class EccPoint():
 
         if not l == r:
             raise ValueError("The EC point does not belong to the curve")
-        
+
     def is_inf(self):
         return self.x == 0 and self.y == 0
 
@@ -39,18 +40,18 @@ class EccPoint():
     @property
     def y(self):
         return self.__y
-    
+
     def __neg__(self):
         negy = (-self.y) % p
         return EccPoint(self.x, negy)
-    
+
     def __add__(self, point):
         if self.is_inf():
             return EccPoint(point.x, point.y)
-        
+
         if point.is_inf():
             return EccPoint(self.x, self.y)
-        
+
         if self == point:
             tmp = pow(2 * self.y, -1, p)
             l = ((3 * self.x ** 2 + a) * tmp) % p
@@ -76,18 +77,20 @@ class EccPoint():
             s = s // 2
 
         return res
-    
+
     def __rmul__(self, left_hand):
         return self.__mul__(left_hand)
-    
+
     def __eq__(self, point):
         return self.x == point.x and self.y == point.y
-    
+
     def __repr__(self):
         return f"({hex(self.x)}, {hex(self.y)})"
 
+
 def EccInf():
     return EccPoint(0, 0)
+
 
 class KeyPair():
     def __init__(self, d, Q):
@@ -97,10 +100,11 @@ class KeyPair():
     @property
     def d(self):
         return self.__d
-    
+
     @property
     def Q(self):
         return self.__Q
+
 
 def get_key_pair():
     d = random.randrange(1, n)
@@ -108,7 +112,8 @@ def get_key_pair():
     Q = d * G
 
     return KeyPair(d, Q)
-    
+
+
 def sign(d, msg):
     order_bits = n.bit_length()
     order_bytes = (order_bits - 1) // 8 + 1
@@ -116,7 +121,7 @@ def sign(d, msg):
     G = EccPoint(Gx, Gy)
 
     h = SHA256.new(msg)
-    z = int.from_bytes(h.digest()[:order_bytes])
+    z = int.from_bytes(h.digest()[:order_bytes], byteorder="big")
 
     r, s = 0, 0
 
@@ -130,11 +135,12 @@ def sign(d, msg):
                 break
 
         invk = pow(k, -1, n)
-        s =  (invk * (z + r * d)) % n
+        s = (invk * (z + r * d)) % n
         if s != 0:
             break
 
     return (r, s)
+
 
 if __name__ == "__main__":
     pass
